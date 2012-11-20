54eb1ddaf2f34d711de7b2e56e8036e7f7e506b1
Ricardo Cabret <ricardo.cabret@gmail.com>
2012-11-20 03:46:44 -0500

    Done, needs a little more change

--- KMeans/src/parseTest.java
+++ KMeans/src/parseTest.java
@@ -1,10 +1,7 @@
 import java.io.FileNotFoundException;
 import java.awt.Color;
 
-
-
 import javax.swing.JPanel;
-
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -23,18 +20,11 @@ import org.jfree.ui.RectangleInsets;
 import org.jfree.ui.RefineryUtilities;
 
 
-
-
-	
-	
-
 public class parseTest{
 	static int k = parseText.k; 
 	static int totalData = parseText.totalData;
-
-   
 	    
-	public static void main(String... aArgs) throws FileNotFoundException {
+		public static void main(String... aArgs) throws FileNotFoundException {
 		
 	    parseText parser = new parseText("iris.data");
 	    parser.processLines();
@@ -48,10 +38,9 @@ public class parseTest{
 	    demo.setVisible(true);
 	   
 	   
-
-	    
-	    for(int i = 0; i < k; i++)
-	    {
+ 
+	    for(int i = 0; i < k; i++){
+	    	
 	        System.out.println("Cluster " + (i+1) + " includes:");
 	        for(int j = 0; j < totalData; j++)
 	        {
@@ -86,124 +75,113 @@ public class parseTest{
 	     XYSeries s6 = new XYSeries("Cluster 3 Centroid");
 	     s6.add(parseText.centroidSet.get(2).getsL(),parseText.centroidSet.get(2).getpL());
 
-	      XYSeries s1 = new XYSeries("Cluster 1");
-	      
-		    
-		       
-		        for(int j = 0; j < totalData; j++)
-		        {
-		            if(parseText.dataSet.get(j).getCluster() == 0){
-		            	
-		            
-	        s1.add(parseText.dataSet.get(j).getsL(),parseText.dataSet.get(j).getpL());
-		        }
-		        }
-		    
-	     XYSeries s2 = new XYSeries("Cluster 2");
-	     for(int j = 0; j < totalData; j++)
-	        {
-	            if(parseText.dataSet.get(j).getCluster() == 1){
-	            	
-	            
-     s2.add(parseText.dataSet.get(j).getsL(),parseText.dataSet.get(j).getpL());
-	        }
-	        }
 	     
-	     XYSeries s3 = new XYSeries("Cluster 3");
-	     for(int j = 0; j < totalData; j++)
-	        {
+	      XYSeries s1 = new XYSeries("Cluster 1");
+	      for(int j = 0; j < totalData; j++)
+	      {
+	    	  if(parseText.dataSet.get(j).getCluster() == 0){
+		            		s1.add(parseText.dataSet.get(j).getsL(),parseText.dataSet.get(j).getpL());
+	    	  }
+	      }
+		    
+	      XYSeries s2 = new XYSeries("Cluster 2");
+	      for(int j = 0; j < totalData; j++){
+	    	  
+	    	  if(parseText.dataSet.get(j).getCluster() == 1){
+	    		  s2.add(parseText.dataSet.get(j).getsL(),parseText.dataSet.get(j).getpL());
+	    	  }
+	      }
+	     
+	      XYSeries s3 = new XYSeries("Cluster 3");
+	      for(int j = 0; j < totalData; j++){
+	    	  
 	            if(parseText.dataSet.get(j).getCluster() == 2){
-	            	
-	            
-     s3.add(parseText.dataSet.get(j).getsL(),parseText.dataSet.get(j).getpL());
-	        }
-	        }
+	            	s3.add(parseText.dataSet.get(j).getsL(),parseText.dataSet.get(j).getpL());
+	            }
+	      }
 	     
 	    
 	     
-	        XYSeriesCollection dataset = new XYSeriesCollection();
-	        dataset.addSeries(s4);
-	        dataset.addSeries(s5);
-	        dataset.addSeries(s6);
-	        dataset.addSeries(s1);
-	        dataset.addSeries(s2);
-	        dataset.addSeries(s3);
+	      XYSeriesCollection dataset = new XYSeriesCollection();
+	        	dataset.addSeries(s4);
+	        	dataset.addSeries(s5);
+	        	dataset.addSeries(s6);
+	        	dataset.addSeries(s1);
+	        	dataset.addSeries(s2);
+	        	dataset.addSeries(s3);
 	       
 	        
-	        return dataset;
+	        	return dataset;
 
 	    }
 
 		    
 	
-	private static void log(Object aObject){
-	    System.out.println(String.valueOf(aObject));
-	  }
+	 private static void log(Object aObject){
+		 System.out.println(String.valueOf(aObject));
+	  	}
 
 	
-public static class plotclust extends ApplicationFrame{
-	private static final long serialVersionUID = 1L;
+	 public static class plotclust extends ApplicationFrame{
+		 private static final long serialVersionUID = 1L;
 
-    {
+		 {
         
-        ChartFactory.setChartTheme(new StandardChartTheme("JFree/Shadow",
+			 ChartFactory.setChartTheme(new StandardChartTheme("JFree/Shadow",
                 true));
-    }
+		 }
 
  
-    public plotclust(String title) {
-        super(title);
-        ChartPanel chartPanel = (ChartPanel) createDemoPanel();
-        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
-        setContentPane(chartPanel);
-    }
+		 public plotclust(String title) {
+			 super(title);
+			 ChartPanel chartPanel = (ChartPanel) createDemoPanel();
+			 chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+			 setContentPane(chartPanel);
+		 }
 
  
-    public JFreeChart createChart(XYDataset dataset) {
+		 public JFreeChart createChart(XYDataset dataset) {
 
       
-    	JFreeChart chart = ChartFactory.createScatterPlot("Iris Data Clustering",
-    			"Sepal Length", "Petal Length", 
-    			dataset,
-    			PlotOrientation.VERTICAL,
-    			true,
-    			true,
-    			false);
+			 JFreeChart chart = ChartFactory.createScatterPlot("Iris Data Clustering",
+					 "Sepal Length", "Petal Length", 
+					 dataset,
+					 PlotOrientation.VERTICAL,
+					 true,
+					 true,
+					 false);
     	
-        chart.setBackgroundPaint(Color.white);
+			 chart.setBackgroundPaint(Color.white);
 
-        XYPlot plot = (XYPlot) chart.getPlot();
-        plot.setBackgroundPaint(Color.lightGray);
-        plot.setDomainGridlinePaint(Color.white);
-        plot.setRangeGridlinePaint(Color.white);
-        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
-        plot.setDomainCrosshairVisible(true);
-        plot.setRangeCrosshairVisible(true);
+			 XYPlot plot = (XYPlot) chart.getPlot();
+			 plot.setBackgroundPaint(Color.lightGray);
+			 plot.setDomainGridlinePaint(Color.white);
+			 plot.setRangeGridlinePaint(Color.white);
+			 plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+			 plot.setDomainCrosshairVisible(true);
+			 plot.setRangeCrosshairVisible(true);
 
-        XYItemRenderer r = plot.getRenderer();
-        if (r instanceof XYLineAndShapeRenderer) {
-            XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) r;
-            renderer.setBaseShapesVisible(true);
-            renderer.setBaseShapesFilled(true);
-            renderer.setDrawSeriesLineAsPath(true);
-        }
+			 XYItemRenderer r = plot.getRenderer();
+			 
+			 if (r instanceof XYLineAndShapeRenderer) {
+				 XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) r;
+				 renderer.setBaseShapesVisible(true);
+				 renderer.setBaseShapesFilled(true);
+				 renderer.setDrawSeriesLineAsPath(true);
+			 }
 
-        return chart;
+			 return chart;
 
-    }
+		 }
 
    
    
-    public JPanel createDemoPanel() {
-        JFreeChart chart = createChart(createDataset());
-        ChartPanel panel = new ChartPanel(chart);
-        panel.setFillZoomRectangle(true);
-        panel.setMouseWheelEnabled(true);
-        return panel;
-    }
-    
-   
-    
-}
-
+		 public JPanel createDemoPanel() {
+			 JFreeChart chart = createChart(createDataset());
+			 ChartPanel panel = new ChartPanel(chart);
+			 panel.setFillZoomRectangle(true);
+			 panel.setMouseWheelEnabled(true);
+			 return panel;
+		 }
+	 }
 }
